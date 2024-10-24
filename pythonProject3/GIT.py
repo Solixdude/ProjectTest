069266d5a727998d37b08a058a192f965a2e573c
Solixdude <roscantonio04@gmail.com>
2024-10-24 19:29:57 +0300

    te

--- pythonProject3/GIT.py
+++ pythonProject3/GIT.py
@@ -1,6 +1,11 @@
 print("Hello world")
 
 x = 20
-if x > 15:
-    print("Hei")
-    
+
+for numar in range(1, x):
+    x -= 1
+    if x > 15:
+        print("Hei")
+    else:
+        print("Hello")
+
